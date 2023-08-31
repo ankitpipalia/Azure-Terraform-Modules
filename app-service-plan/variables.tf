2a0872c3fb6379138f7c63b81b626cb1a6e12c3d
Vishwaas <vicky1352002@gmail.com>
2023-08-31 16:53:24 +0530

    add App Service Plan Module

---
+++ app-service-plan/variables.tf
@@ -0,0 +1,15 @@
+variable "App_Service_Plan_Config" {
+  description = "A list of maps containing configurations for App Service Plans"
+  type  = map(object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    os_type             = string
+    sku                 = string
+  }))
+}
+
+# variable "App_Service_Plan_Config" {
+#   description = "A list of maps containing configurations for App Service Plans"
+#   type        = list(map(any))
+#  }
--- resource-group/main.tf
+++ resource-group/main.tf
@@ -1,5 +1,5 @@
 locals {
-  resource_group_name = "${var.names.project_name}-${var.location}"
+  resource_group_name = "${var.names.project_name}_${var.location}_${var.names.environment}"
 
   unique_name = var.unique_name == "true" ? random_integer.suffix[0].result : (var.unique_name == "false" ? null : var.unique_name)
 }
@@ -12,7 +12,8 @@ resource "random_integer" "suffix" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = local.unique_name != null ? "${local.resource_group_name}-${local.unique_name}" : local.resource_group_name
+  # name     = local.unique_name != null ? "${local.resource_group_name}-${local.unique_name}" : local.resource_group_name
+  name     = "${var.names.project_name}-${var.names.location}-${var.names.environment}"
   location = var.location
   tags     = var.tags
 }
