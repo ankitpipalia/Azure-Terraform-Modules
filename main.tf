be843a1c0d9556e950c9ceff683023a949f7ae11
Ankit Pipalia <ankit.pipalia009@gmail.com>
2023-09-06 22:28:18 +0530

    few changes

--- main.tf
+++ main.tf
@@ -31,109 +31,116 @@ module "resource_group" {
   custom_tags = local.custom_tags
 }
 
-module "virtual_network" {
-  source = "./modules/Networking/Virtual Networks"
+module "storage_acount" {
+  source = "./modules/Storage/storage-account"
 
-  virtual_network_name = "test-vnet"
-  location = module.resource_group.location
-  resource_group_name = module.resource_group.name
-  address_space = ["10.0.0.0/16"]
-  tags = local.tags
-  custom_tags = local.custom_tags
+  
+  
 }
 
-module "subnets" {
-  source = "./modules/Networking/subnets"
+# module "virtual_network" {
+#   source = "./modules/Networking/Virtual Networks"
+`
+#   virtual_network_name = "test-vnet"
+#   location = module.resource_group.location
+#   resource_group_name = module.resource_group.name
+#   address_space = ["10.0.0.0/16"]
+#   tags = local.tags
+#   custom_tags = local.custom_tags
+# }
 
-  for_each = toset(local.subnets)
-  subnet_name = each.value
-  resource_group_name = module.resource_group.name
-  virtual_network_name = module.virtual_network.name
-  subnet_address_prefix = cidrsubnet(module.virtual_network.address_space[0], 8, index(local.subnets, each.value))  # Ensure unique address for each subnet
-  # For example, if you're given a prefix ending in /16 and you want your subnets to have a prefix of /20, then newbits would be 4, because 20 - 16 = 4
-}
+# module "subnets" {
+#   source = "./modules/Networking/subnets"
 
-module "public_ip_address" {
-  source = "./modules/Networking/public-ip"
+#   for_each = toset(local.subnets)
+#   subnet_name = each.value
+#   resource_group_name = module.resource_group.name
+#   virtual_network_name = module.virtual_network.name
+#   subnet_address_prefix = cidrsubnet(module.virtual_network.address_space[0], 8, index(local.subnets, each.value))  # Ensure unique address for each subnet
+#   # For example, if you're given a prefix ending in /16 and you want your subnets to have a prefix of /20, then newbits would be 4, because 20 - 16 = 4
+# }
 
-  public_ip_name = "test-pip"
-  resource_group_name = module.resource_group.name
-  location = module.resource_group.location
-  allocation_method = "Static"
-  sku = "Standard"
-  tags = local.tags
-  custom_tags = local.custom_tags
-}
+# module "public_ip_address" {
+#   source = "./modules/Networking/public-ip"
 
-module "network_interface" {
-  source = "./modules/Networking/network-interface"
+#   public_ip_name = "test-pip"
+#   resource_group_name = module.resource_group.name
+#   location = module.resource_group.location
+#   allocation_method = "Static"
+#   sku = "Standard"
+#   tags = local.tags
+#   custom_tags = local.custom_tags
+# }
 
-  network_interface_name = "test-nic"
-  resource_group_name = module.resource_group.name
-  location = module.resource_group.location
-  ip_configuration_name = "testconfiguration1"
-  subnet_id = module.subnets["subnet1"].id
-  private_ip_address_allocation = "Dynamic"
-  private_ip_address = null
-  public_ip_address_id = module.public_ip_address.id
-  tags = local.tags
-  custom_tags = local.custom_tags
-}
+# module "network_interface" {
+#   source = "./modules/Networking/network-interface"
 
-module "network_security_group" {
-  source = "./modules/Networking/network-security-group"
+#   network_interface_name = "test-nic"
+#   resource_group_name = module.resource_group.name
+#   location = module.resource_group.location
+#   ip_configuration_name = "testconfiguration1"
+#   subnet_id = module.subnets["subnet1"].id
+#   private_ip_address_allocation = "Dynamic"
+#   private_ip_address = null
+#   public_ip_address_id = module.public_ip_address.id
+#   tags = local.tags
+#   custom_tags = local.custom_tags
+# }
 
-  network_security_group_name = "test-nsg"
-  resource_group_name = module.resource_group.name
-  location = module.resource_group.location
-  inbound_rules = [
-    {
-      name                       = "SSH"
-      priority                   = 100
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_address_prefix      = "*"
-      source_port_range          = "*"
-      destination_address_prefix = "*"
-      destination_port_range     = "22"
-      description                = "Allow SSH"
-    }
-  ]
-  outbound_rules = [
-    {
-      name                       = "AllowInternetOutBound"
-      priority                   = 100
-      access                     = "Allow"
-      protocol                   = "*"
-      source_address_prefix      = "*"
-      source_port_range          = "*"
-      destination_address_prefix = "*"
-      destination_port_range     = "*"
-      description                = "Allow Internet OutBound"
-    }
-  ]
-  subnet_id = module.subnets["subnet1"].id  
+# module "network_security_group" {
+#   source = "./modules/Networking/network-security-group"
 
-  tags = local.tags
-  custom_tags = local.custom_tags
-}
+#   network_security_group_name = "test-nsg"
+#   resource_group_name = module.resource_group.name
+#   location = module.resource_group.location
+#   inbound_rules = [
+#     {
+#       name                       = "SSH"
+#       priority                   = 100
+#       access                     = "Allow"
+#       protocol                   = "Tcp"
+#       source_address_prefix      = "*"
+#       source_port_range          = "*"
+#       destination_address_prefix = "*"
+#       destination_port_range     = "22"
+#       description                = "Allow SSH"
+#     }
+#   ]
+#   outbound_rules = [
+#     {
+#       name                       = "AllowInternetOutBound"
+#       priority                   = 100
+#       access                     = "Allow"
+#       protocol                   = "*"
+#       source_address_prefix      = "*"
+#       source_port_range          = "*"
+#       destination_address_prefix = "*"
+#       destination_port_range     = "*"
+#       description                = "Allow Internet OutBound"
+#     }
+#   ]
+#   subnet_id = module.subnets["subnet1"].id  
 
-module "virtual_machine" {
-  source = "./modules/Compute/virtual-machine"
+#   tags = local.tags
+#   custom_tags = local.custom_tags
+# }
 
-  virtual_machine_name = "test-vm"
-  resource_group_name = module.resource_group.name
-  location = module.resource_group.location
-  vm_size = "Standard_B1ls"
-  admin_username = "testadmin"
-  admin_password = "Password1234!"
-  network_interface_id = module.network_interface.id
-  source_image_publisher = "Canonical"
-  source_image_offer     = "0001-com-ubuntu-minimal-focal"
-  source_image_sku       = "minimal-20_04-lts-gen2"
-  source_image_version   = "latest"
-  depends_on = [ module.network_interface ]
+# module "virtual_machine" {
+#   source = "./modules/Compute/virtual-machine"
 
-  tags = local.tags
-  custom_tags = local.custom_tags
-}
+#   virtual_machine_name = "test-vm"
+#   resource_group_name = module.resource_group.name
+#   location = module.resource_group.location
+#   vm_size = "Standard_B1ls"
+#   admin_username = "testadmin"
+#   admin_password = "Password1234!"
+#   network_interface_id = module.network_interface.id
+#   source_image_publisher = "Canonical"
+#   source_image_offer     = "0001-com-ubuntu-minimal-focal"
+#   source_image_sku       = "minimal-20_04-lts-gen2"
+#   source_image_version   = "latest"
+#   depends_on = [ module.network_interface ]
+
+#   tags = local.tags
+#   custom_tags = local.custom_tags
+# }
